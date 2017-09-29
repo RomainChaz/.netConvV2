faa3c5971a5896b0f832b87576c8b4e3322477df
Rominou <romainchazottier@gmail.com>
2017-09-29 16:51:47 +0200

    first commit

---
+++ ClientConvertisseurV2/Service/WSService.cs
@@ -0,0 +1,52 @@
+﻿using ClientConvertisseurV2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientConvertisseurV2.Service
+{
+    public class WSService
+    {
+
+        private static HttpClient client = HttpClientSingleton.getInstance();
+
+        public WSService()
+        {
+
+        }
+
+        public static async Task<List<Devise>> getAllDevisesAsync()
+        {
+            List<Devise> devises = new List<Devise>();
+
+            HttpResponseMessage response = await client.GetAsync("Devise");
+            if (response.IsSuccessStatusCode)
+            {
+                devises = await response.Content.ReadAsAsync<List<Devise>>();
+            }
+
+            return devises;
+        }
+    }
+
+    public class HttpClientSingleton
+    {
+        private static HttpClient client;
+        
+        public static HttpClient getInstance()
+        {
+            if (client == null)
+            {
+                client = new HttpClient();
+                client.BaseAddress = new Uri("http://localhost:1721/api/");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            }
+            return client;
+        }
+    }
+}
---
+++ ClientConvertisseurV2/ViewModel/ViewModelLocator.cs
@@ -0,0 +1,29 @@
+﻿using GalaSoft.MvvmLight.Ioc;
+using Microsoft.Practices.ServiceLocation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace ClientConvertisseurV2.ViewModel
+{
+    /// <summary>
+    /// This class contains static references to all the view models in the
+    /// application and provides an entry point for the bindings.
+    /// <para>
+     /// See http://www.mvvmlight.net
+     /// </para>
+     /// </summary>
+     public class ViewModelLocator
+        {
+            static ViewModelLocator()
+            {
+                ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
+                SimpleIoc.Default.Register<MainViewModel>();
+            }
+            /// <summary>
+            /// Gets the Main property.
+            /// </summary>
+            public MainViewModel Main => ServiceLocator.Current.GetInstance<MainViewModel>();
+        }
+}
